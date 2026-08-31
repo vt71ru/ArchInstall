935eedda0fed0db483fa2365d429445d72f53c8c
vt71ru <69967509+vt71ru@users.noreply.github.com>
2026-08-31 12:00:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -350,43 +350,13 @@ load_core_libraries()
         config.sh
 
     require_lib \
-        utils.sh
+        logger.sh
 
     require_lib \
-        terminal.sh
-
-    require_lib \
-        cursor.sh
-
-    require_lib \
-        colors.sh
-
-    require_lib \
-        unicode.sh
+        common.sh
 
     require_lib \
         tui.sh
-
-    require_lib \
-        screen.sh
-
-    require_lib \
-        draw.sh
-
-    require_lib \
-        input.sh
-
-    require_lib \
-        events.sh
-
-    require_lib \
-        widgets.sh
-
-    require_lib \
-        dialog.sh
-
-    require_lib \
-        progress.sh
 
     logger_info \
         "Core libraries loaded"
